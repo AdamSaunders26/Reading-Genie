a6ce058f723dbf34b7e38ce999b22a38df16742b
adamsaunders26 <a.saunders206@gmail.com>
2023-11-25 11:05:25 +0000

    toggle interests

--- app/signin/SignIn-2.tsx
+++ app/signin/SignIn-2.tsx
@@ -11,21 +11,50 @@ export default function SignIn2({
 }: {
   setCurrentStage: React.Dispatch<React.SetStateAction<number>>;
 }) {
-  // const toggleInterest = (section, interest) => {
-  //   if (!selected.hasOwnProperty(section)) {
-  //     selected[section] = {};
-  //   }
-  //   if (selected[section].hasOwnProperty(interest)) {
-  //     delete selected[section][interest]
-  //   } else {
-  //     selected[section][interest] = true;
-  //   }
+  // @ts-ignore
+  const toggleInterest = (section, interest) => {
+    if (!selected.hasOwnProperty(section)) {
+      // @ts-ignore
+      selected[section] = {};
+    }
+    // @ts-ignore
+    if (selected[section].hasOwnProperty(interest)) {
+      // @ts-ignore
+      delete selected[section][interest];
+    } else {
+      // @ts-ignore
+      selected[section][interest] = true;
+    }
 
-  //   console.log(selected);
+    console.log(selected);
+  };
 
-  // }
-
-  const selected = {};
+  const [selected, setSelected] = useState({
+    interests: {
+      sport: false,
+      film: false,
+      bakeoff: false,
+      celebrities: false,
+      animals: false,
+      pirates: false,
+      dinosaurs: false,
+      dancing: false,
+      gymnastics: false,
+    },
+  });
+  // const selected = {
+  //   interests: {
+  //     sport: false,
+  //     film: false,
+  //     bakeoff: false,
+  //     celebrities: false,
+  //     animals: false,
+  //     pirates: false,
+  //     dinosaurs: false,
+  //     dancing: false,
+  //     gymnastics: false,
+  //   },
+  // };
 
   const interests = [
     "sport",
@@ -49,25 +78,36 @@ export default function SignIn2({
 
   const clicked = "bg-red-500";
   const notClicked = "bg-blue-500";
-
+  console.log(selected);
   return (
     <div className="flex flex-col m-4 p-4 gap-4">
       <h1 className="text-3xl text-center">What do they care about?</h1>
       <div className="grid grid-cols-3 gap-4">
-        {interests.map((interest, idx) => (
-          <Button
-            key={idx}
-            // onClick={() => toggleInterest("interests", interest)}
-            className="border border-black text-black bg-white hover:bg-gray-300"
-          >
-            {interest}
-          </Button>
-        ))}
+        {interests.map((interest, idx) => {
+          return (
+            <Button
+              key={idx}
+              // @ts-ignore
+              className={selected.interests[interest] ? clicked : notClicked}
+              onClick={() => {
+                setSelected((curr) => {
+                  const newSelected = { ...curr, ...interests };
+                  newSelected.interests[interest] = true;
+                  return newSelected;
+                });
+                // toggleInterest("interests", interest);
+              }}
+              // className="border border-black text-black bg-white hover:bg-gray-300"
+            >
+              {interest}
+            </Button>
+          );
+        })}
       </div>
       <h1 className="text-3xl text-center">What content to they enjoy?</h1>
       {contentTypes.map((contentType, idx) => (
         <Button
-          // onClick={() => toggleInterest("contentTypes", contentType)}
+          onClick={() => toggleInterest("contentTypes", contentType)}
           key={idx}
           className="border border-black text-black bg-white hover:bg-gray-300"
         >
@@ -77,7 +117,7 @@ export default function SignIn2({
       <h1 className="text-3xl text-center">Length of content</h1>
       {Object.keys(contentLengths).map((key, idx) => (
         <Button
-          // onClick={() => toggleInterest("contentLengths", contentLengths[key])}
+          onClick={() => toggleInterest("contentLengths", contentLengths[key])}
           key={idx}
           className="border border-black text-black bg-white hover:bg-gray-300"
         >
