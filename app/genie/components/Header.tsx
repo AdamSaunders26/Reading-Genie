f7a9138d4df45f4a8a8cd4f1fe41ff2d3d493eff
adamsaunders26 <a.saunders206@gmail.com>
2024-04-08 10:49:44 +0100

    update header design

--- app/genie/components/Header.tsx
+++ app/genie/components/Header.tsx
@@ -6,21 +6,25 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center shadow-lg bg-primary w-full">
-      <div className="flex gap-2">
+    <header className="flex  items-center justify-center   w-full">
+      {/* <div className="flex gap-2">
         <Image
           src={lamp}
           alt="reading genie lamp"
           className="w-20 -ml-2 pt-1"
         />
         <p className="text-white -ml-3 pt-1">0</p>
-      </div>
+      </div> */}
       <Link href="/">
-        <Image src={textlogo} alt="reading genie" className="w-64 pl-6" />
+        <Image
+          src={textlogo}
+          alt="reading genie"
+          className="w-64  m-2 pr-2 pl-3 shadow-lg bg-primary rounded-full"
+        />
       </Link>
-      <div className="w-24 flex items-center justify-center">
-        {/* <SettingsButton /> */}
-      </div>
+      {/* <div className="w-24 flex items-center justify-center">
+        <SettingsButton />
+      </div> */}
     </header>
   );
 }
